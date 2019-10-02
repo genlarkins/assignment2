8681783eccfbdcd0c6f0c9dfd558440d0d061f02
Genevieve Larkins <gtl16@my.fsu.edu>
2019-10-02 17:38:17 -0400

    Updated Jet.h
    Added member data and member functions to Jet.h

---
+++ Jet.h
@@ -0,0 +1,21 @@
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle {
+	private:
+		int numberOfEngines;
+	public: 
+		Jet();
+		explicit Jet(string brand, string model, string fuelType, int numEngines);
+    		virtual ~Jet();
+    		int getnumEngines();
+    		void setNumberOfEngines(int numEngines);
+    		virtual double mileageEstimate(double time);
+    		virtual string toString();
+
+
+};
+
+#endif
