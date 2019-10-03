fd7785d59d225958493f9ddfc30f3369edbf6fe4
Genevieve Larkins <gtl16@my.fsu.edu>
2019-10-03 12:17:54 -0400

    Member data added
    Additional member data/functions added to Train.h

--- Train.h
+++ Train.h
@@ -7,12 +7,15 @@ class Train : public PoweredVehicle {
 
 private:
     int towWeight;
+    string cargotype;
 
 public:
 
-    explicit Train(string brand, string model, string fuelType, int towWeight);
+    explicit Train(string brand, string model, string fuelType, int towWeight, string cargotype);
     virtual ~Train();
+    string getcargoType();
     int gettowWeight();
+    void setcargoType(string cargotype);
     void settowWeight(int towWeight);
     virtual double mileageEstimate(double time);
     virtual string toString();
