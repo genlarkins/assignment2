f5689866171b8fb62f66889ea2f2d1fcc912c135
Genevieve Larkins <gtl16@my.fsu.edu>
2019-10-03 10:47:50 -0400

    All functions added to Jet.cpp
    Additional functions were added to Jet.cpp to complete it.

--- Jet.cpp
+++ Jet.cpp
@@ -1,4 +1,5 @@
 #include "Jet.h"
+#include <random>
 
 Jet::Jet(string brand, string model, string fuelType, int numberOfEngines) {
 	setBrand(brand);
@@ -18,5 +19,19 @@ void Jet::setNumberOfEngines(int numEngines) {
 }
 
 double Jet::mileageEstimate(double time) {
+	std::random_device rd; // obtain a random number from hardware
+	std::mt19937 eng(rd()); // seed the generator
+	std::uniform_int_distribution<> distr(40, 100); // define the range
+	int randomnum = distr(eng);
+	//credit for code that generates random number: https://stackoverflow.com/questions/7560114/random-number-c-in-some-range
 
+
+	double mileage = randomnum * time;
+	if(fuelType == "Rocket" && numberOfEngines >= 3)
+		mileage += mileage * .055;
+	return mileage;
+}
+
+string Jet::toString(){
+	return "-> Jet\n" + PoweredVehicle::toString() + "\n\tNumber of Engines: " + getnumEngines();
 }
